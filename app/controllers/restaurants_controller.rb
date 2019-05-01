bc23ef87c8d85a3a1bcbaca5d06d29feaf9fda9f
Tiago Leal <tiago.sleal12@gmail.com>
2019-05-01 09:31:33 -0300

    set method index, show in restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,28 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: :show
+
   def index
+    @restaurants = Restaurant.near(params[:city] || 'São Paulo')
+    filter_by_category if params[:category]
+    render json: @restaurants
   end
 
   def show
+    render json: @restaurant, product_categories: true
   end
 
   def search
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
   end
 end
